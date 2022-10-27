e6f8fc335eeee2e82ba4245dffe6021cec2bd9e4
brenley-woodard <brenleyew@gmail.com>
2022-10-27 14:46:48 -0700

    psueocode for JS DOM elements, events, and functions

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-/* Imports */
-
 /* Get DOM Elements */
-
-/* State */
+// submit button
+// prompt section
+// fortune section
+// fortune output
+// reset button
 
 /* Events */
+// event listener for submit button that targets fortuneP
+// event listener for reset button that clears image and answer
 
 /* Display Functions */
-
-// (don't forget to call any display functions you want to run on page load!)
+// function that randomly generates fortune output
+// function(s) that toggle appropriate hidden objects
