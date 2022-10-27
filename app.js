bb7452f99557348f642f0541c01412ba36ce2f2a
brenley-woodard <brenleyew@gmail.com>
2022-10-27 15:17:25 -0700

    event listener for submit button logs to console

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /* Get DOM Elements */
 // submit button
+const submitButton = document.getElementById('submit-button');
+const promptSection = document.getElementById('prompt');
 // prompt section
 // fortune section
 // fortune output
@@ -7,6 +9,9 @@
 
 /* Events */
 // event listener for submit button that targets fortuneP
+submitButton.addEventListener('click', () => {
+    console.log('I am being clicked');
+});
 // event listener for reset button that clears image and answer
 
 /* Display Functions */
